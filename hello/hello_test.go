e86ab80880d5e110f025812f0404c94e98511eab
Tammy Husnetdinova <tamhus31@gmail.com>
2024-04-29 16:47:11 -0600

    Update hello_test.go

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,9 @@ package hello_test
 import "testing"
 import "github.com/t-husnetdinova/GoLangDemo/hello"
 
+// sample test function (it will fail, since this is the wrong greeting)
 func TestHello(t *testing.T) {
-	if hello.HelloWorld() != "Hey Planet" {
+	if hello.HelloWorld() != "Hello World!!! :)" {
 		t.Fatal("wrong greeting :(")
 	}
 }
