7f2e8dae5abf7282b2f87e5d79fe519a06c70489
Tammy Husnetdinova <tamhus31@gmail.com>
2024-04-30 10:49:23 -0600

    added value conversion example

--- main.go
+++ main.go
@@ -5,6 +5,21 @@ import "github.com/t-husnetdinova/GoLangDemo/hello"
 import "rsc.io/quote"
 
 func main() {
+	// call HelloWorld function
 	fmt.Println(hello.HelloWorld())
+
+	
+	// test out quote function
 	fmt.Println(quote.Go())
+
+
+	// type conversion example
+	speed := 100 // int
+	force := 2.5 // float64
+	// first convert speed to float64, then convert it back to int to assing to itself
+	speed = int(float64(speed) * force)
+	fmt.Println(speed) 
+
+
+	//
 }
